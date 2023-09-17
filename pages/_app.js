3be76eae7b5c0be0d4d27e0d180526b2036391e4
Hand <shamushand@gmail.com>
2023-09-17 12:58:20 -0700

    Fix warnings

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,17 @@
+import React from 'react'
+import Head from 'next/head'
 import '../styles.css'
 
 function MyApp({ Component, pageProps })
 {
   return (
-    <Component {...pageProps} />
+    <>
+      <Head>
+        <title>Kana Coach</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
   )
 }
 
--- pages/_document.js
+++ pages/_document.js
@@ -6,18 +6,7 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <meta charSet="utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" type="image/x-icon" href="favicon.ico" />
-          <title>Kana Coach</title>
-          <link 
-            rel="preconnect" 
-            href="https://fonts.googleapis.com"
-          />
-          <link 
-            rel="preconnect" 
-            href="https://fonts.gstatic.com" 
-            crossOrigin
-          />
           <link 
             href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&family=Roboto:wght@100&display=swap" 
             rel="stylesheet"
