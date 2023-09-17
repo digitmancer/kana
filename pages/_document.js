87f1e92ed408fe9d42e602d67dbb21ad6004b076
Hand <shamushand@gmail.com>
2023-09-17 12:07:36 -0700

    Add favicon

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,8 @@ class MyDocument extends Document {
         <Head>
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <title>Kana</title>
+          <link rel="icon" type="image/x-icon" href="favicon.ico" />
+          <title>Kana Coach</title>
           <link 
             rel="preconnect" 
             href="https://fonts.googleapis.com"
