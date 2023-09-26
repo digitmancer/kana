78d092b9aa2c9fb0d63753fda89036c89183f7ee
dustman-hash <shamushand@gmail.com>
2023-09-25 17:57:54 -0700

    Add alternate romanizations, missing digraphs

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,16 @@
 import dynamic from 'next/dynamic';
 import { useEffect, useState, useRef } from 'react';
-import { hiragana, hiraganaDigraphs, katakana, katakanaDigraphs } from '../data/kana';
+import { romanizations } from '../data/kana';
+import { hiragana, katakana, hiraganaDigraphs, katakanaDigraphs } from '../data/userdata';
 
-function AnswerField({ rightAnswer, onRightAnswer })
+function AnswerField({ romaji, onRightAnswer })
 {
   const inputRef = useRef(null);
   const [input, setInput] = useState('');
   const [missed, setMissed] = useState(false);
 
-  const pronunciation = new Audio('/audio/' + rightAnswer + '.wav');
-  
+  const pronunciation = new Audio('/audio/' + romaji.hepburn + '.wav');
+
   useEffect(() => 
   {    
     const focusInput = () => inputRef.current.focus();
@@ -20,9 +21,10 @@ function AnswerField({ rightAnswer, onRightAnswer })
 
   const handleInput = (event) => 
   {
+    const isRightAnswer = (answer) => Object.values(romaji).includes(answer);
     const value = event.target.value.toLowerCase().trim();
-    if (value === 'n' &&
-        value === rightAnswer)
+    if (value === 'n' && 
+        isRightAnswer(value))
     {
         handleRightAnswer();
         return;
@@ -31,7 +33,7 @@ function AnswerField({ rightAnswer, onRightAnswer })
     if (value.length === 3 ||
         ['a', 'e', 'i', 'o', 'u'].includes(value.charAt(value.length - 1)))
     {
-      if (value === rightAnswer)
+      if (isRightAnswer(value))
         handleRightAnswer();
       else
         handleWrongAnswer();
@@ -61,7 +63,7 @@ function AnswerField({ rightAnswer, onRightAnswer })
       type="text"
       value={input}
       ref={inputRef}
-      placeholder={missed ? rightAnswer : ''}
+      placeholder={missed ? romaji.hepburn : ''}
       onChange={handleInput}
       autoComplete="off"
       autoFocus
@@ -77,10 +79,10 @@ function App()
   return (
     <div className="wrapper">
       <div className="prompt">
-        {question.prompt}
+        {question.kana}
       </div>
       <AnswerField 
-        rightAnswer={question.answer} 
+        romaji={question.romaji} 
         onRightAnswer={getNewQuestion}
       />
     </div>
@@ -89,11 +91,25 @@ function App()
 
 function getQuestion()
 {
-  const questions = [...hiragana, ...katakana, ...hiraganaDigraphs, ...katakanaDigraphs];
-  const question = questions[Math.floor(Math.random() * questions.length)];
-  return { 
-    prompt: question.kana, 
-    answer: question.pronunciation 
+  const questions = 
+  [
+    ...hiragana, 
+    ...katakana, 
+    ...hiraganaDigraphs, 
+    ...katakanaDigraphs
+  ];
+
+  const totalWeight = questions.reduce((total, question) => total + question.weight, 0);
+  let index = totalWeight * Math.random();
+  const question = questions.find(element => 
+  {
+    index -= element.weight;
+    return index <= 0;
+  });
+
+  return {
+    kana:   question.kana,
+    romaji: romanizations[question.kana]
   };
 }
 
